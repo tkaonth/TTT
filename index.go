6063eec12ce23d58b351c08e28d0a4703bd14350
tkaonth <tkaonth@gmail.com>
2021-07-30 11:05:06 +0700

    gg

--- index.go
+++ index.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"net/http"
+
+	lib "./lib"
 
 	_ "github.com/go-sql-driver/mysql"
 	"golang.org/x/crypto/bcrypt"
@@ -11,6 +12,11 @@ import (
 
 var db *sql.DB
 var err error
+
+type Users struct {
+	username string
+	password string
+}
 
 func main() {
 	db, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/testing")
@@ -25,6 +31,7 @@ func main() {
 	http.HandleFunc("/signup", signupPage)
 	http.HandleFunc("/login", loginPage)
 	http.HandleFunc("/", homePage)
+	lib.Globalprint()
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -45,29 +52,32 @@ func signupPage(res http.ResponseWriter, req *http.Request) {
 		http.ServeFile(res, req, "signup.html")
 		return
 	}
-	username := req.FormValue("username")
-	password := req.FormValue("password")
-	var user string
-	fmt.Println("username : " + username)
-	fmt.Println("pasword : " + password)
-	fmt.Println("user : " + user)
-	err := db.QueryRow("SELECT username FROM users WHERE username=?", username).Scan(&user)
-	fmt.Println("error : " + err.Error())
+	var user Users
+	user = Users{
+		username: req.FormValue("username"),
+		password: req.FormValue("password"),
+	}
+	var userdb string
+	//fmt.Println("username : " + user.username)
+	//fmt.Println("password : " + user.password)
+	err := db.QueryRow("SELECT username FROM users WHERE username=?", user.username).Scan(&userdb)
+	//fmt.Println("userdb : " + userdb)
 	switch {
 	case err == sql.ErrNoRows:
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.password), bcrypt.DefaultCost)
 		if err != nil {
 			http.Error(res, "Server error,unable to create your accout", 500)
 			return
 		}
-		_, err = db.Exec("INSERT INTO users(username,password) VALUES (?,?)", username, hashedPassword)
+		_, err = db.Exec("INSERT INTO users(username,password) VALUES (?,?)", user.username, hashedPassword)
 		if err != nil {
 			http.Error(res, "Server error,unable to create your accout", 500)
 			return
 		}
 		res.Write([]byte("User created!"))
+		//http.Redirect(res, req, "/", 200)
 		return
-	case err != nil:
+	case err == nil:
 		http.Error(res, "Server error,unable to create your accout", 500)
 		return
 	default:
---
+++ lib/lib.go
@@ -0,0 +1,11 @@
+package lib
+
+import "fmt"
+
+func localprint() {
+	fmt.Println("local")
+}
+
+func Globalprint() {
+	fmt.Println("Global")
+}
